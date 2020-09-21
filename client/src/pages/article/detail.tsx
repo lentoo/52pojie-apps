35fb946680ad56b8b9ab3d4f807cdb860266dda6
lentoo <729533020@qq.com>
2020-09-22 00:41:30 +0800

    feat: 🎸 文章评论列表

--- client/src/pages/article/detail.tsx
+++ client/src/pages/article/detail.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import Taro, { getCurrentInstance } from '@tarojs/taro'
-import { View, Button, RichText, Text} from '@tarojs/components';
+import { View, Button, Image, RichText, Text} from '@tarojs/components'
 
 import './detail.scss'
-import { Article, } from 'src/types/plate';
+import { Article, ArticleCommentItem, ArticleComment } from 'src/types/plate';
 import UserInfo from './components/UserInfo'
 
 type ArticleDetailProp = {
@@ -12,18 +12,54 @@ type ArticleDetailProp = {
 }
 type ArticleDetailState = {
   result?: Article
+  comments: ArticleCommentItem[]
+  loadingText: string
 }
 
 export default class ArticleDetail extends Component<ArticleDetailProp, ArticleDetailState> {
   constructor(props) {
     super(props)
     this.state = {
-      result: undefined
+      result: undefined,
+      comments: [],
+      loadingText: '正在加载中'
     }
   }
+  $page = 1
+  hasNext = false
   componentDidMount() {
     this.fetchData()
   }
+  onReachBottom() {
+    console.log('onReachBottom');
+    if (this.hasNext) {
+      this.fetchComments()
+    }
+  }
+
+  fetchComments() {
+    const { result } = this.state
+    if (!result) return
+    Taro.cloud.callFunction({
+      name: 'home',
+      data: {
+        action: 'get_article_detail_comments_data',
+        id: result.id,
+        page: ++this.$page
+      }
+    }).then(res => {
+      const data = res.result as ArticleComment
+      console.log(data)
+      this.setState({
+        comments: [...this.state.comments!, ...data.comments]
+      })
+      this.hasNext = data.hasNext
+      this.setState({
+        loadingText: this.hasNext ? '正在加载中' : '没有更多了'
+      })
+    })
+  }
+  
   fetchData() {
     const link = getCurrentInstance().router?.params.link
     if (!link) {
@@ -42,17 +78,20 @@ export default class ArticleDetail extends Component<ArticleDetailProp, ArticleD
       data: {
         action: 'get_article_detail_data',
         url: link,
-        page: 1
+        page: this.$page
       }
     })
       .then(res => {
         const result = res.result as Article
         console.log(result);
         this.setState({
-          result
+          result,
+          comments: result.comments
         })
-
-        
+        this.hasNext = result.hasNext
+        this.setState({
+          loadingText: this.hasNext ? '正在加载中' : '没有更多了'
+        })
       })
       .finally(() => {
         Taro.hideLoading()
@@ -88,7 +127,7 @@ export default class ArticleDetail extends Component<ArticleDetailProp, ArticleD
     }
   }
   render() {
-    const { result } = this.state
+    const { result, comments, loadingText } = this.state
     if (!result) {
       return <View></View>
     }
@@ -105,6 +144,34 @@ export default class ArticleDetail extends Component<ArticleDetailProp, ArticleD
             </View>
           </View>
           <RichText className='article-detail-body' nodes={`<div>${result.content}</div>`}></RichText>
+        </View>
+
+        <View className='article-comments'>
+          <View className='comments-title'>评论</View>
+          <View className='comments-list'>
+            {
+              comments.map((item, index) => {
+                return (
+                  <View key={index} className='comments-item'>
+                    <View className='flex ali-center'>
+                      <Image className='comments-item-avatar' src={item.avatar} />
+                      <View>
+                        <View className='comments-item-name'>{item.username}</View>
+                        <View className='comments-item-date'>{item.post_date}</View>
+                      </View>
+                    </View>
+                    <RichText className='comments-item-content' nodes={item.content}></RichText>
+                    {/* <View className='comments-item-content'>{item.content}</View> */}
+                  </View>
+                )
+              })
+            }
+
+            <View className='loading-more'>
+              <Text>{loadingText}</Text>
+            </View>
+            
+          </View>
         </View>
 
         <Button className='btn-float' onClick={this.previewImages}>+</Button>
